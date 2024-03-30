913c39745294b76e5e42a8690ed944a7092a48f1
Priyansu Mukherjee <84275293+D4rD00M@users.noreply.github.com>
2024-03-30 22:06:39 +0530

    Update SentinelScan.py

--- SentinelScan/SentinelScan.py
+++ SentinelScan/SentinelScan.py
@@ -1,6 +1,5 @@
 import socket
 import sys
-
 common_ports = {
     21: "FTP",
     22: "SSH",
@@ -10,16 +9,12 @@ common_ports = {
     80: "HTTP",
     443: "HTTPS"
 }
-
-# Function to scan for open ports
 def scan(target):
     print("Scanning ports on target:", target)
     open_ports = []
-
     for port in common_ports.keys():
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1) 
-
         try:
             result = sock.connect_ex((target, port))
             if result == 0:
@@ -35,16 +30,10 @@ def scan(target):
         except socket.error:
             print("Could not connect to server.")
             sys.exit()
-
     return open_ports
-
-# Main function
 def main():
-    # Names of creators
-    main_creator = "Xer0Weeds"
-    helping_creators = ["Z3nith", "kraven"]
-
-    # Prompt user for target IP address or hostname
+    main_creator = "D4rD00M"
+    helping_creators = ["Z3nithn", "kraven"]
     target = input("Enter the target IP address or hostname: ")
     open_ports = scan(target)
 
@@ -56,11 +45,7 @@ def main():
                 print("CVE-2022-1234: Example vulnerability")
     else:
         print("No open ports found. Target appears to be secure.")
-
-    # Stylishly display creator names
     print("\nScript created by:", main_creator)
     print("Assistance provided by:", ", ".join(helping_creators))
-
-# Execute the main function if the script is run directly
 if __name__ == "__main__":
     main()
